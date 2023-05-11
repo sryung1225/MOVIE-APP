2e2c0abf70f649169853292a0ca3faecb19d6d51
sryung <sryung1225@gmail.com>
2023-05-12 01:51:00 +0900

    [👖] Typing the Props - interface - components의 props에 type 주기

---
+++ typescript/src/Circle.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+interface ContainerProps {
+  bgColor: string;
+}
+
+const Continer = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 50%;
+`;
+
+interface CircleProps {
+  bgColor: string;
+}
+
+function Circle({ bgColor }: CircleProps) {
+  return <Continer bgColor={bgColor} />;
+}
+
+export default Circle;
+
+// practice interface -------------------------------------
+interface PlayerShape {
+  name: string;
+  age: number;
+}
+
+const sayHello = (playerObj: PlayerShape) =>
+  `Hello ${playerObj.name} you are ${playerObj.age} years old.`;
+
+sayHello({ name: "nico", age: 12 });
+sayHello({ name: "ryung", age: 26 });
