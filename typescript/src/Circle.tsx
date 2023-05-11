98cedd611427972d4bb4433d93b3100b30547ac2
sryung <sryung1225@gmail.com>
2023-05-12 02:28:29 +0900

    [👖] Optional Props - 선택적인 props가 있고, 그 props를 styled-components에서도 사용하는 경우 대응하기

--- typescript/src/Circle.tsx
+++ typescript/src/Circle.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 const Continer = styled.div<ContainerProps>`
@@ -9,26 +10,33 @@ const Continer = styled.div<ContainerProps>`
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 50%;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
+  text?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Continer bgColor={bgColor} />;
+function Circle({ bgColor, borderColor, text = "기본텍스트" }: CircleProps) {
+  return (
+    <Continer bgColor={bgColor} borderColor={borderColor ?? "black"}>
+      {text}
+    </Continer>
+  );
 }
 
 export default Circle;
 
 // practice interface -------------------------------------
-interface PlayerShape {
-  name: string;
-  age: number;
-}
+// interface PlayerShape {
+//   name: string;
+//   age: number;
+// }
 
-const sayHello = (playerObj: PlayerShape) =>
-  `Hello ${playerObj.name} you are ${playerObj.age} years old.`;
+// const sayHello = (playerObj: PlayerShape) =>
+//   `Hello ${playerObj.name} you are ${playerObj.age} years old.`;
 
-sayHello({ name: "nico", age: 12 });
-sayHello({ name: "ryung", age: 26 });
+// sayHello({ name: "nico", age: 12 });
+// sayHello({ name: "ryung", age: 26 });
