1025de7eaa009f32174f4552d125c9585f5e82ef
sryung <sryung1225@gmail.com>
2023-05-12 18:51:07 +0900

    [👖] Form - React와 TS를 이용해서 form 다루기

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,10 +1,28 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e; // const value = event.currentTarget.value;
+    setValue(value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("hello", value);
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="white" />
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
