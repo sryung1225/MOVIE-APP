d144bef8c36788a6362ab3cec48255bcc9de71cb
sryung <sryung1225@gmail.com>
2023-05-12 19:13:11 +0900

    [👖] Themes - React와 TS, styled-component를 이용해서 테마 변경 다루기

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,29 +1,16 @@
-import { useState } from "react";
+import { styled } from "styled-components";
 
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e; // const value = event.currentTarget.value;
-    setValue(value);
-  };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("hello", value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          value={value}
-          onChange={onChange}
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
