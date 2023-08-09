6bc74da18425a492d087a9b803d82ed9095a7edf
sryung <sryung1225@gmail.com>
2023-08-09 17:10:16 +0900

    [📚] EFFECT : counter React앱 만들기 복습 - strict mode 해제

--- cra-for-beginners/src/App.js
+++ cra-for-beginners/src/App.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("Render!");
   return (
     <div>
-      <h1>Welcome</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
