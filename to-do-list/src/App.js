77bcd3d06120a53585ea636ca86865cbffb158fa
sryung <sryung1225@gmail.com>
2023-08-11 23:52:02 +0900

    [📚✔] TO-DO-LIST : 할 일 목록 추가하기

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => {
+    setToDo(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  const checkToDos = () => {
+    console.log(toDos);
+  };
+  useEffect(checkToDos, [toDos]);
+  return (
+    <div>
+      <h1>My To Dos</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="할 일을 작성하세요"
+        />
+        <button>할 일 추가</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
