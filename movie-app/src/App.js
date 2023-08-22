a3cbf3a01593490025fde96bdab2d26bfc1aaa3e
sryung <sryung1225@gmail.com>
2023-08-23 01:10:48 +0900

    [📚🎬] MOVIE-APP : Home 과 Detail 연결하기

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home.js";
+import Detail from "./routes/Detail.js";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie({ key, coverImg, title, summary, genres }) {
   return (
     <div key={key}>
       <img src={coverImg} alt={`<${title}> poster`} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
